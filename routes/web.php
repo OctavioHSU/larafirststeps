bcc92c1452f74433761a817d1b672affb19e9159
OctavioHS <122421806+OctavioHS@users.noreply.github.com>
2024-06-22 22:16:15 -0600

    #1 - Agregaro rutas de prueba

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Route::get('/test', function () {
+    return "Welcome";
+}); */
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+/* Route::get('/crud', function () {
+    return view('crud/index');
+}); */
+
+Route::get('/crud', function () {
+    $age = 28;
+    $data = ['name' => 'Octavio', 'age' => $age];
+
+    return view('crud/index', $data);
+})->name('crud');
