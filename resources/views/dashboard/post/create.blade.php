3820571948ff2105311d5a19047719bba11b047f
OctavioHS <122421806+OctavioHS@users.noreply.github.com>
2024-07-12 09:40:16 -0600

    #12 - Agregar vista general post

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -1,23 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('dashboard.layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Crear Post</title>
-</head>
-
-<body>
+@section('content')
     <h1>Crear Post</h1>
 
-    @if ($errors->any())
-        @foreach ($errors->all() as $e)
-            <div class="error">
-                {{ $e }}
-            </div>
-        @endforeach
-    @endif
+    @include('dashboard.fragment._errors-form')
 
     <form action="{{ route('post.store') }}" method="POST">
         @csrf
@@ -49,7 +35,4 @@
 
         <button type="submit">Enviar</button>
     </form>
-
-</body>
-
-</html>
+@endsection
